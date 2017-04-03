96ce69e7e0036d136c8f101237cfe5a6e7a8ca38
Oleg Dreyman <dreymonde@me.com>
2017-04-03 16:53:51 +0300

    AutoProcessor

--- Sources/Lane.swift
+++ Sources/Lane.swift
@@ -139,3 +139,58 @@ public struct Processor<Key : Hashable, Value> : ProcessorProtocol {
     }
     
 }
+
+public protocol AutoProcessorProtocol {
+    
+    associatedtype Key : Hashable
+    associatedtype Value
+    
+    func start(key: Key, completion: @escaping ProcessorCompletion<Value>)
+    func cancel(key: Key)
+    func cancelAll()
+    
+}
+
+public extension AutoProcessorProtocol {
+    
+    func processor() -> Processor<Key, Value> {
+        return Processor(AutoProcessor(self))
+    }
+    
+}
+
+public final class AutoProcessor<Proc : AutoProcessorProtocol> : ProcessorProtocol {
+    
+    public typealias Key = Proc.Key
+    public typealias Value = Proc.Value
+    
+    private let proc: Proc
+    
+    public init(_ autoProcessor: Proc) {
+        self.proc = autoProcessor
+    }
+    
+    private var tasks: Synchronized<[Key : ProcessingState]> = .init([:])
+    
+    public func start(key: Proc.Key, completion: @escaping (ProcessorResult<Proc.Value>) -> ()) {
+        tasks.set({ (dict : inout [Key : ProcessingState]) in dict[key] = .running })
+        proc.start(key: key) { [weak self] (result) in
+            self?.tasks.set({ (dict : inout [Key : ProcessingState]) in dict[key] = .completed })
+            completion(result)
+        }
+    }
+    
+    public func processingState(key: Proc.Key) -> ProcessingState {
+        return tasks.get()[key] ?? .none
+    }
+    
+    public func cancel(key: Proc.Key) {
+        proc.cancel(key: key)
+        tasks.set({ (dict : inout [Key : ProcessingState]) in dict[key] = nil })
+    }
+    
+    public func cancelAll() {
+        proc.cancelAll()
+    }
+    
+}
